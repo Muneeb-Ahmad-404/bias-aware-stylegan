9cfe16e7aca34313565ac5c4927562bfdc6dd972
Muneeb <muneebahmad2005p@gmail.com>
2026-07-20 22:37:49 +0500

    feat: add data ingestion script

---
+++ src/data/ingest.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import shutil
+import yaml
+import kagglehub
+
+
+class DatasetIngestor:
+    def __init__(self, config_path: str):
+        self.config = self._load_config(config_path)
+        self.raw_path = Path(self.config["paths"]["raw"])
+
+        self.raw_path.mkdir(parents=True, exist_ok=True)
+
+    def _load_config(self, config_path):
+        with open(config_path, "r") as file:
+            return yaml.safe_load(file)
+
+    def download_dataset(self, dataset_name, dataset_config):
+        print(f"Downloading {dataset_name}")
+
+        downloaded_path = Path(
+            kagglehub.dataset_download(
+                dataset_config["kaggle"]
+            )    
+        )
+
+        destination = self.raw_path / dataset_config["name"]
+
+        if destination.exists():
+            print(f"{destination} already exists")
+            return
+
+        shutil.copytree(
+            downloaded_path,
+            destination,
+            dirs_exist_ok=True
+        )
+
+        print(f"Saved to {destination}")
+
+    def run(self):
+        for name, dataset in self.config["datasets"].items():
+            self.download_dataset(name, dataset)
+
+
+if __name__ == "__main__":
+    ingestor = DatasetIngestor(
+        "configs/config.yaml"
+    )
+
+    ingestor.run()
